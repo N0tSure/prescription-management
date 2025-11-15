f53e5556426600af4113565320b29599d0f8fdc7
Artem Sirosh <asir2089@gmail.com>
2025-11-15 22:14:28 +0100

    POST-001 Add dummy Spring Boot project

---
+++ src/main/java/dev/artemsirosh/prescription/PrescriptionManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.artemsirosh.prescription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/artemsirosh/prescription/PrescriptionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.artemsirosh.prescription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
